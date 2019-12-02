2e3be6365f6b51c581e53e660fcff7a2764de448
smeshkov <smeshkow@gmail.com>
2019-12-02 18:12:28 +1100

    Do not use short install command, use line per dep instead

--- _bin/deps.sh
+++ _bin/deps.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
 sudo apt-get update
-sudo apt-get -y install libsdl2{,-image,-mixer,-ttf,-gfx}-dev
-go get -v github.com/veandco/go-sdl2/{sdl,img,mix,ttf}
+
+sudo apt-get -y install libsdl2-image-dev
+sudo apt-get -y install libsdl2-mixer-dev
+sudo apt-get -y install libsdl2-ttf-dev
+sudo apt-get -y install libsdl2-gfx-dev
+
+go get -v github.com/veandco/go-sdl2/sdl
+go get -v github.com/veandco/go-sdl2/img
+go get -v github.com/veandco/go-sdl2/mix
+go get -v github.com/veandco/go-sdl2/ttf
+go get -v github.com/veandco/go-sdl2/gfx
