f607fe6fa4e16ab7d8ec69c3c6eb4d0aa9fdd488
smeshkov <smeshkow@gmail.com>
2019-12-02 17:03:27 +1100

    Added CI to provide ready to use releases

---
+++ _bin/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+DIST_DIR="_dist"
+BINARY="trovehero"
+OS="$1"
+VERSION="$2"
+
+if [ -z "$OS" ]; then
+    OS="darwin"
+fi
+
+if [ ! -z "$VERSION" ]; then
+    VERSION="_$VERSION"
+fi
+
+if [ ! -d "$DIST_DIR" ]; then
+  mkdir -p $DIST_DIR
+fi
+
+env GOOS=${OS} GOARCH=amd64 go build -v -o ${DIST_DIR}/${BINARY}_${OS}${VERSION} .
---
+++ _bin/install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+DIST_DIR="_dist"
+BINARY="trovehero"
+USER_BIN=$HOME/bin
+OS="$1"
+VERSION="$2"
+
+if [ -z "$OS" ]; then
+    OS="darwin"
+fi
+
+if [ ! -z "$VERSION" ]; then
+    VERSION="_$VERSION"
+fi
+
+PATH=${DIST_DIR}/${BINARY}_${OS}${VERSION}
+
+chmod +x ${PATH}
+
+if [ ! -d "$USER_BIN" ]; then
+  mkdir -p ${USER_BIN}
+  echo "created $USER_BIN directory, don't forget to add it to PATH environment variable"
+fi
+
+mv ${PATH} ${USER_BIN}/${BINARY}
