29e9a4a657952f8f85b9e7f1c090744db76e8b34
smeshkov <smeshkow@gmail.com>
2019-12-03 20:28:37 +1100

    CI wars

--- _bin/build.sh
+++ _bin/build.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-bash deps.sh
+sudo apt-get update
+
+sudo apt-get -y install libsdl2-image-dev
+sudo apt-get -y install libsdl2-mixer-dev
+sudo apt-get -y install libsdl2-ttf-dev
+sudo apt-get -y install libsdl2-gfx-dev
+
+go get -v github.com/veandco/go-sdl2/sdl
+go get -v github.com/veandco/go-sdl2/img
+go get -v github.com/veandco/go-sdl2/mix
+go get -v github.com/veandco/go-sdl2/ttf
+go get -v github.com/veandco/go-sdl2/gfx
 
 DIST_DIR="_dist"
 BINARY="trovehero"
